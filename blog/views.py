baa6f524405c4f06a9b8a1e60fd91aa1058ea6fc
jefferson1707 <jeffersonfelipemc@hotmail.com>
2025-12-19 18:46:32 -0300

    Debug: views.py

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post, Category, Comment
 from .forms import CommentForm, PostForm  
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
 # Criar views para o blog.
 
